de5e68cf6dafc1f3a31d4895e6aba10bd3ab71fa
kaptsov <kaptsovaa@gmail.com>
2022-03-17 20:43:47 +0400

    первичное добавление классов

--- tg_food_plan_bot/models.py
+++ tg_food_plan_bot/models.py
@@ -8,13 +8,51 @@ class Customers(models.Model):
 
 
 class Preferences(models.Model):
-    type = models.CharField(max_length=30, verbose_name = "Предпочтение")
+    type = models.CharField(max_length=30, verbose_name="Предпочтение")
 
 
 class Subscription(models.Model):
-    owner = models.ForeignKey(user, on_delete=models.CASCADE, verbose_name="Подписка")
+    owner = models.ForeignKey(Customers, on_delete=models.CASCADE, verbose_name="Подписка")
     register_date = models.DateField()
     paid_until = models.DateField()
     person_amount = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name="Количество персон")
-    preferences = models.ForeignKey(preferences, on_delete=models.CASCADE, versbose_name="Предпочтение")
+    preferences = models.ForeignKey(Preferences, on_delete=models.CASCADE, versbose_name="Предпочтение")
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Название ингредиента")
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Название блюда")
+    description = models.TextField()
+    preferences = models.ManyToManyField(
+        Preferences,
+        thorugh="RecipeClassificator",
+        through_fields=("recipe", "preferences"),
+        )
+    ingredients = models.ManyToManyField(
+        Ingredients,
+        through="RecipeIngredients",
+        through_fields=("recipe", "ingredient"),
+    )
+
+
+class RecipeClassificator(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    preferences = models.ForeignKey(Preferences, on_delete=models.CASCADE)
+
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    ingredient_amount = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name="Количество ингредиента")
+    ingredient_measure = models.CharField(max_length=30, verbose_name="Единица измерения")
+
+
+
+
+
+
+
 
