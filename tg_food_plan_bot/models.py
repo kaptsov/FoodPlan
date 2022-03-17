a8babc9c1b98ea10393b8e3453ac140e78593404
Michael <soplomiha@gmail.com>
2022-03-17 17:40:41 +0300

    Добавил модель Пользователя

--- tg_food_plan_bot/models.py
+++ tg_food_plan_bot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    telegram_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
