198d847b7f2b315519090e7b82424471540496a1
Michael <soplomiha@gmail.com>
2022-03-17 17:46:50 +0300

    Переименовал User в  Customer

--- tg_food_plan_bot/models.py
+++ tg_food_plan_bot/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+class Customer(models.Model):
     telegram_id = models.IntegerField()
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
