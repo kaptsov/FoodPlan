240c8a4c56d47911c18ee287880e6f3bd3c28e67
kaptsov <kaptsovaa@gmail.com>
2022-03-18 18:56:08 +0400

    Связи добавлены в админку, названия изменены на единственное число

--- tg_food_plan_bot/admin.py
+++ tg_food_plan_bot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer
+from .models import Preference
+from .models import Subscription
+from .models import Ingredient
+from .models import Recipe
+from .models import RecipeClassificator
+from .models import RecipeIngredient
+
+admin.site.register(Customer)
+admin.site.register(Preference)
+admin.site.register(Subscription)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(RecipeClassificator)
+admin.site.register(RecipeIngredient)
