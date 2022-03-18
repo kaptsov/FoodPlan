75fb9cfc7033cd7ac55d7baf09d7be7b2548c4a2
kaptsov <kaptsovaa@gmail.com>
2022-03-18 18:40:47 +0400

    Исправлены опечатки

--- tg_food_plan_bot/models.py
+++ tg_food_plan_bot/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Customers(models.Model):
-    username = models.CharField(max_length=25, verbose_name = 'Имя')
-    phone_number = models.CharField(max_length=30, verbose_name = "Телефон")
+    username = models.CharField(max_length=25, verbose_name='Имя')
+    phone_number = models.CharField(max_length=30, verbose_name="Телефон")
     telegram_id = models.PositiveIntegerField(verbose_name="ID пользователя в телеграмме", unique=True)
 
 
@@ -15,7 +16,7 @@ class Subscription(models.Model):
     register_date = models.DateField()
     paid_until = models.DateField()
     person_amount = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name="Количество персон")
-    preferences = models.ForeignKey(Preferences, on_delete=models.CASCADE, versbose_name="Предпочтение")
+    preferences = models.ForeignKey(Preferences, on_delete=models.CASCADE, verbose_name="Предпочтение")
 
 
 class Ingredients(models.Model):
@@ -27,14 +28,14 @@ class Recipe(models.Model):
     description = models.TextField()
     preferences = models.ManyToManyField(
         Preferences,
-        thorugh="RecipeClassificator",
+        through="RecipeClassificator",
         through_fields=("recipe", "preferences"),
         )
     ingredients = models.ManyToManyField(
         Ingredients,
         through="RecipeIngredients",
         through_fields=("recipe", "ingredient"),
-    )
+        )
 
 
 class RecipeClassificator(models.Model):
